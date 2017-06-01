55f33ea713f036545036216c6544cc8e5e306c1d
Chris W Jones <chris@christopherjones.us>
2017-06-01 16:49:58 -0400

    Migrated in twitch

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
+ruby "2.4.1"
 source 'https://rubygems.org'
 
 gem 'rails', '~> 5.0.3'
@@ -5,6 +6,7 @@ gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'haml-rails'
 
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
--- app/assets/javascripts/twitch.js
+++ app/assets/javascripts/twitch.js
@@ -1,2 +1,39 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function updateLayout(userData){
+  var row = $("<div>").addClass("row");
+  var user = $("<div>").addClass("two").addClass("columns").text(userData.name);
+  var status = $("<div>").addClass("two").addClass("columns").text("offline");
+  var otherInfo = $("<div>").addClass("eight").addClass("columns").text(userData.bio);
+  row.append(user);
+  row.append(status);
+  row.append(otherInfo);
+
+  $("#users").empty().append(row);
+}
+
+function getUpdates(users){
+  _.each(users, function(user){
+    userData = {name: "", status: "", bio: ""};
+    $.getJSON("https://wind-bow.glitch.me/twitch-api/users/" + user, function(userStream){
+     
+      userData.name = userStream.display_name;
+      userData.bio = userStream.bio;
+      updateLayout(userData);
+    })
+  })
+}
+
+$(function(){
+  var users = ["freecodecamp", "ewokichka"];
+  
+  getUpdates(users);
+  
+  $("form").on("submit", function(e){
+    e.preventDefault();
+    var newUser = $("#user-add").val();
+    users.push(newUser);
+    getUpdates(users);    
+    return false;
+  })
+})
