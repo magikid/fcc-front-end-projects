daaf8b42c0fef7066b48b8f1ca3ad553ea2611fe
Chris W Jones <chris@christopherjones.us>
2017-06-06 20:38:20 -0400

    Reworks twitch html and js to actually work
    With this commit, the twitch page actually works.  It queries the twitch
    api for info about the users and displays it using Ember.

--- app/assets/javascripts/twitch.js
+++ app/assets/javascripts/twitch.js
@@ -1,12 +1,28 @@
 window.App = Ember.Application.create({
   rootElement: '#ember',
   LOG_TRANSITIONS: true,
-  LOG_TRANSITIONS_INTERNAL: true
 });
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    names = ["freecodecamp", "ewokichka"];
+    output = [];
+    names.forEach(function(name){
+      output.push(App.getUser(name));
+    });
+    return Ember.RSVP.all(output);
+  }
+});
+
+App.getUser = function(name){
+  return $.ajax({
+    dataType: "json",
+    url: 'https://wind-bow.glitch.me/twitch-api/users/'.concat(name),
+  }).then()
+};
+
 
 App.Router.map(function() {
+  this.route('addUser');
 });
 
-App.IndexController = App.IndexController.extend({
-  firstName: "Chris"
-});
