4d41352011e1c0a49578a21e655626c6ff2ff81d
Chris W Jones <chris@christopherjones.us>
2017-06-06 21:40:20 -0400

    Refactors twitch JS to use Ember's REST Adapter
    This changes how the data is fetched from the twitch API.  Since my
    server is proxying the calls and formatting the data the way Ember-Data
    expects, I'm able to take advantage of the internal datastore provided
    by Ember-Data.

--- app/assets/javascripts/twitch.js
+++ app/assets/javascripts/twitch.js
@@ -1,26 +1,36 @@
 window.App = Ember.Application.create({
   rootElement: '#ember',
-  LOG_TRANSITIONS: true,
+});
+
+App.User = DS.Model.extend({
+  display_name: DS.attr(),
+  name: DS.attr(),
+  bio: DS.attr(),
+  logo: DS.attr(),
+})
+
+App.UserSerializer = DS.RESTSerializer.extend({
+  primaryKey: 'name',
+});
+
+App.UserAdapter = DS.RESTAdapter.extend({
+  host: '/api/twitch',
+  primaryKey: 'name',
+  queryRecord: function(modelName, query) {
+    return Ember.$.getJSON("/api/twitch/users/".concat(arguments[2]));
+  }
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     names = ["freecodecamp", "ewokichka"];
     output = [];
-    names.forEach(function(name){
-      output.push(App.getUser(name));
-    });
+    for(var i=0; i<names.length; i++){
+      output.push(this.get('store').queryRecord('user', names[i]));
+    };
     return Ember.RSVP.all(output);
   }
 });
-
-App.getUser = function(name){
-  return $.ajax({
-    dataType: "json",
-    url: 'https://wind-bow.glitch.me/twitch-api/users/'.concat(name),
-  }).then()
-};
-
 
 App.Router.map(function() {
   this.route('addUser');
